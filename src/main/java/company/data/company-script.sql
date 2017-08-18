96e46eee872e4c04e1e2d8485b9ad0925ad8a846
sagittaria <shuyang_x@hotmail.com>
2017-08-18 13:48:27 +0800

    改数据库结构，给Employee加年龄

--- src/main/java/company/data/company-script.sql
+++ src/main/java/company/data/company-script.sql
@@ -10,9 +10,27 @@ create table EmployeeTbl
 id int not null AUTO_INCREMENT,
 name varchar(255),
 email varchar(255),
+age int(11),
 primary key (id)
 ); 
-insert into EmployeeTbl(name,email) values('甲姓名','a@b.c');
-insert into EmployeeTbl(name,email) values('乙姓名','d@e.f');
-insert into EmployeeTbl(name,email) values('丙姓名','g@h.i');
-insert into EmployeeTbl(name,email) values('丁姓名','j@k.l');
+insert into EmployeeTbl(name,email,age) values('甲姓名','a0@b.c',10);
+insert into EmployeeTbl(name,email,age) values('甲姓名','a1@b.c',12);
+insert into EmployeeTbl(name,email,age) values('乙姓名','d0@e.f',12);
+insert into EmployeeTbl(name,email,age) values('乙姓名','d1@e.f',16);
+insert into EmployeeTbl(name,email,age) values('乙姓名','d2@e.f',17);
+insert into EmployeeTbl(name,email,age) values('乙姓名','d3@e.f',23);
+insert into EmployeeTbl(name,email,age) values('丙姓名','g0@h.i',17);
+insert into EmployeeTbl(name,email,age) values('丁姓名','j0@k.l',17);
+insert into EmployeeTbl(name,email,age) values('丁姓名','j1@k.l',17);
+insert into EmployeeTbl(name,email,age) values('丁姓名','j2@k.l',23);
+
+--    select age, count(*) from employeetbl group by age;
+--      +------+----------+
+--      | age  | count(*) |
+--      +------+----------+
+--      |   10 |        1 |
+--      |   12 |        2 |
+--      |   16 |        1 |
+--      |   17 |        4 |
+--      |   23 |        2 |
+--      +------+----------+
